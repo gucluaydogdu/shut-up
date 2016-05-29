4d1bf31a9143c1fc93de5acfa7c3fcbcd57fe7de
Güçlü Aydoğdu <guclu.aydogdu@gmail.com>
2016-05-29 22:34:11 +0300

    Initial Commit

---
+++ ShutUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShutUp
+//
+//  Created by Güçlü Aydoğdu on 29/05/16.
+//  Copyright © 2016 Güçlü Aydoğdu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
