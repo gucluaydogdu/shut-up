59048efe08d0670347e548109c086868d5f3939b
Güçlü Aydoğdu <guclu@Guclus-MacBook-Pro-2.local>
2016-06-03 00:00:58 +0300

    added files first time

--- ShutUp/ViewController.swift
+++ ShutUp/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var suEcem: UIButton!
+    @IBOutlet weak var bbGuclu: UIButton!
+    @IBOutlet weak var Ecem: UIImageView!
+    @IBOutlet weak var Guclu: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ShutUp(sender: AnyObject) {
+        Ecem.hidden = true
+    }
+    @IBAction func ByeBye(sender: AnyObject) {
+        Guclu.hidden = true
+    }
 
 }
 
